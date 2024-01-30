c17b74af9af7fe9c7095220bc974f3a71830f983
Daniel <70210356+Itsnotaka@users.noreply.github.com>
2024-01-30 11:22:46 +0800

    [feat] added redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/x",
+        destination: "https://x.com/d2ac__",
+        permanent: true,
+      },
+      {
+        source: "/github",
+        destination: "https://github.com/itsnotaka",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/nameisdaniel/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
